79dee1607dfce72a2252d6ed4150cb2f2f68cc0c
Satyam Garg <113013930+garg2002@users.noreply.github.com>
2023-12-22 07:25:24 +0530

    Update ThreadSyn.java

--- ThreadSyn.java
+++ ThreadSyn.java
@@ -60,7 +60,7 @@ public class ThreadSyn {
     public static void main(String[] args) {
         Account a1 = new Account(5000);
         Customer c1 = new Customer(a1, "Satyam");
-        Customer c2 = new Customer(a1, "Surbhi");
+        Customer c2 = new Customer(a1, "Shivam");
         Thread t1 = new Thread(c1);
         Thread t2 = new Thread(c2);
         t1.start();
